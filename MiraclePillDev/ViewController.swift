1baf52cc6101a9b754f7b09089115ce98ec12e0d
William Fulton <will.fulton95@gmail.com>
2017-01-27 23:21:22 -0500

    statePicker is working and functions have been put into viewcontroller

--- MiraclePillDev/ViewController.swift
+++ MiraclePillDev/ViewController.swift
@@ -8,10 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    @IBOutlet weak var statePicker: UIPickerView!
+    @IBOutlet weak var stateButton: UIButton!
+  
+    let states = ["alaska", "alabama", "kansas", "delaware", "michigan", "virginia", "california", "hawaii"]
+        
     override func viewDidLoad() {
         super.viewDidLoad()
+        statePicker.dataSource = self
+        statePicker.delegate = self
         
     }
 
@@ -20,6 +27,32 @@ class ViewController: UIViewController {
         
     }
 
-
+    @IBAction func stateButtonPressed(_ sender: Any) {
+        
+        stateButton.isHidden = true
+        
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return states.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return states[row]
+    
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+        stateButton.setTitle(states[row], for: UIControlState.normal)
+        
+    }
+    
 }
 
+//WHY WONT PICKER VIEW SHOW UP 
+//ASK AT CODE AND COFFEE!!!!!!!!!!!!!
